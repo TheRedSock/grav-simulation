1635509d03728ec5b8a53465a94d0c92527c199a
TheRedSock <torstein.rosok@gmail.com>
2014-10-28 19:13:18 +0100

    Update script.js
    Prøver meg på http://en.wikipedia.org/wiki/Elastic_collision#Two-Dimensional_Collision_With_Two_Moving_Objects

--- script.js
+++ script.js
@@ -284,12 +284,33 @@ function checkCollision(i) {
 	 				var m1 = circleArray[i].r;
 	 				var m2 = circleArray[j].r;
 
+					/* Prøver meg på formel:
+					   http://en.wikipedia.org/wiki/Elastic_collision#Two-Dimensional_Collision_With_Two_Moving_Objects
+				 	   ser også på:
+				 	   http://www.hoomanr.com/Demos/Elastic2/
+				 	   Det fungerer ikke, men noen ganger sender det i riktig retning. ??? */
+				 	   
+	 				var d1 = u1x * u1y * Math.cos(Math.atan2(u1y, u1x));
+	 				var d2 = u2x * u2y * Math.cos(Math.atan2(u2y, u2x));
+	 				var A = Math.atan2((y1 - y2), (x1 - x2));
+
+	 				var cosd1A = Math.cos(d1 - A);
+	 				var cosd2A = Math.cos(d2 - A);
+	 				var sind1A = Math.sin(d1 - A);
+	 				var sind2A = Math.sin(d2 - A);
+
+	 				circleArray[i].vx = ((((u1x * cosd1A * (m1 - m2)) + (2 * m2 * u2x * cosd2A)) / (m1 + m2)) * Math.cos(A)) + (u1x * sind1A * Math.cos(A + (Math.PI / 2)));
+	 				circleArray[i].vy = ((((u1y * cosd1A * (m1 - m2)) + (2 * m2 * u2y * cosd2A)) / (m1 + m2)) * Math.sin(A)) + (u1y * sind1A * Math.sin(A + (Math.PI / 2)));
+
+	 				circleArray[j].vx = ((((u2x * cosd2A * (m2 - m1)) + (2 * m1 * u1x * cosd1A)) / (m2 + m1)) * Math.cos(A)) + (u2x * sind2A * Math.cos(A + (Math.PI / 2)));
+	 				circleArray[j].vy = ((((u2y * cosd2A * (m2 - m1)) + (2 * m1 * u1y * cosd1A)) / (m2 + m1)) * Math.sin(A)) + (u2y * sind2A * Math.sin(A + (Math.PI / 2)));
+	 				
 	 				/* En dimensjonal elastisk støt formel for hver vektor.
 	 				   Dette er ikke riktig formel. */
-	 				circleArray[i].vx = ((u1x * (m1 - m2)) + (2 * (m2 * u2x))) / (m1 + m2);
+	 				/*circleArray[i].vx = ((u1x * (m1 - m2)) + (2 * (m2 * u2x))) / (m1 + m2);
 	 				circleArray[j].vx = ((u2x * (m2 - m1)) + (2 * (m1 * u1x))) / (m1 + m2);
 	 				circleArray[i].vy = ((u1y * (m1 - m2)) + (2 * (m2 * u2y))) / (m1 + m2);
-	 				circleArray[j].vy = ((u2y * (m2 - m1)) + (2 * (m1 * u1y))) / (m1 + m2);
+	 				circleArray[j].vy = ((u2y * (m2 - m1)) + (2 * (m1 * u1y))) / (m1 + m2);*/
 
 	 				// For å legge friksjon på støt mellom kuler.
 	 				/*circleArray[i].vx *= bounceFactor;  
